a8812ce7783dc17afd64991e2c85188e9d7f9a4f
Merve Dursun <30603038+merveyanar@users.noreply.github.com>
2026-06-30 09:57:22 +0300

    Add files via upload

---
+++ recipes.js
@@ -0,0 +1,35 @@
+const recipes = [
+  {
+    id: 1,
+    title: "Mercimek Çorbası",
+    ingredients: [
+      "1 su bardağı kırmızı mercimek",
+      "1 soğan",
+      "1 havuç"
+    ],
+    instructions:
+      "Mercimeği yıkayın. Soğan ve havucu kavurun. Su ekleyip pişirin."
+  },
+  {
+    id: 2,
+    title: "Menemen",
+    ingredients: [
+      "3 yumurta",
+      "2 domates",
+      "2 biber"
+    ],
+    instructions:
+      "Biberleri kavurun, domatesleri ekleyin. Yumurtaları kırıp karıştırın."
+  },
+  {
+  id: 3,
+  title: "Sütlaç",
+  ingredients: [
+    "1 litre süt",
+    "1 çay bardağı pirinç",
+    "Şeker"
+  ],
+  instructions:
+    "Pirinci haşlayın, süt ve şekeri ekleyip pişirin."
+}
+];
